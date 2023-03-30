079afba80f70fc1a42f8c19dd275794a63cbb296
wibetter <365533093@qq.com>
2023-03-30 18:13:51 +0800

    fix: 微调下英语语料

--- src/locale/en-US.ts
+++ src/locale/en-US.ts
@@ -3,12 +3,12 @@ import { extendLocale } from 'i18n-runtime';
 extendLocale('en-US', {
   abc052102786d375f2b0d6f41fa13051:
     'Amis is a low code front-end framework that uses JSON configuration to generate pages, which can reduce page development workload and greatly improve efficiency.',
-  '52dfce669769762026d17a75a55a1577': 'Lofta · Low code platform',
+  '52dfce669769762026d17a75a55a1577': 'aisuda · Low code platform',
   b05c95f75e82f460c79f127094388f20: 'review',
   '742e6a42d39a711535da712320e90b90': 'React component',
   '687fbf4d0a505403a102f105b4a8eff3': 'Information display card',
   f1d4ff50f3828f9b73412e7d94e6dd6e: 'custom',
-  '224e2ccda861c2514faa683b3683c361': 'to configure',
+  '224e2ccda861c2514faa683b3683c361': 'configure',
   '74a3905ca8d767198bb95aeb8cf9d295': 'Card title',
   f02e24e6526f93503052618562a22184: 'Show Pictures',
   '7798dfb863ef3e454cb56e5ac7a79796': 'Number of pictures',
